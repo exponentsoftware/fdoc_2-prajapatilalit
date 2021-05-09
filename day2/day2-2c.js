743157d4a0411720de90e9370e2f4f53e776f2fe
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-09 17:38:45 +0530

    update function for adding user skills if user present

---
+++ day2/day2-2c.js
@@ -0,0 +1,55 @@
+const users = [
+  {
+    name: "Brook",
+    scores: 75,
+    skills: ["HTM", "CSS", "JS"],
+    age: 16,
+  },
+  {
+    name: "Alex",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "David",
+    scores: 75,
+    skills: ["HTM", "CSS"],
+    age: 22,
+  },
+  {
+    name: "John",
+    scores: 85,
+    skills: ["HTM"],
+    age: 25,
+  },
+  {
+    name: "Sara",
+    scores: 95,
+    skills: ["HTM", "CSS", "JS"],
+    age: 26,
+  },
+  {
+    name: "Martha",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "Thomas",
+    scores: 90,
+    skills: ["HTM", "CSS", "JS"],
+    age: 20,
+  },
+];
+
+const userName = "Martha";
+//Add skills if user is  present
+function addSkills(userData) {
+  const findUser = userData.filter((user) =>
+    user.name === userName ? user.skills.push("Reactjs") : ""
+  );
+  //log find user data after adding
+  console.log(findUser);
+}
+addSkills(users);
