68f55633acf8ae7b2aba41dd681750e38b2f474a
prajapatilalit <prajapatilalit150992@gmail.com>
2021-05-09 11:48:51 +0530

    update function which return greater then 85 score of the array object

---
+++ day2/day2-1.js
@@ -0,0 +1,26 @@
+const countries = [
+  "ALBANIA",
+  "BOLIVIA",
+  "CANADA",
+  "DENMARK",
+  "ETHIOPIA",
+  "FINLAND",
+  "GERMANY",
+  "HUNGARY",
+  "IRELAND",
+  "JAPAN",
+  "KENYA",
+];
+createArrayOfArrays(countries)[
+  (["Albania", "ALB", 7],
+  ["Bolivia", "BOL", 7],
+  ["Canada", "CAN", 6],
+  ["Denmark", "DEN", 7],
+  ["Ethiopia", "ETH", 8],
+  ["Finland", "FIN", 7],
+  ["Germany", "GER", 7],
+  ["Hungary", "HUN", 7],
+  ["Ireland", "IRE", 7],
+  ["Japan", "JAP", 5],
+  ["Kenya", "KEN", 5])
+];
---
+++ day2/day2-2a.js
@@ -0,0 +1,52 @@
+const users = [
+  {
+    name: "Brook",
+    scores: 75,
+    skills: ["HTM", "CSS", "JS"],
+    age: 16,
+  },
+  {
+    name: "Alex",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "David",
+    scores: 75,
+    skills: ["HTM", "CSS"],
+    age: 22,
+  },
+  {
+    name: "John",
+    scores: 85,
+    skills: ["HTM"],
+    age: 25,
+  },
+  {
+    name: "Sara",
+    scores: 95,
+    skills: ["HTM", "CSS", "JS"],
+    age: 26,
+  },
+  {
+    name: "Martha",
+    scores: 80,
+    skills: ["HTM", "CSS", "JS"],
+    age: 18,
+  },
+  {
+    name: "Thomas",
+    scores: 90,
+    skills: ["HTM", "CSS", "JS"],
+    age: 20,
+  },
+];
+
+function scoresGreaterThan85(usersData) {
+  const scoreGreaterThen85Users = usersData.filter(
+    (score) => score.scores > 85
+  );
+  console.log(scoreGreaterThen85Users);
+}
+scoresGreaterThan85(users);
